195c95e9ed22dac0b4dd4db57e18d4d58bec3645
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-04-14 16:48:33 +0200

    AKBS: Optimize build framework v2
    *Make paths more clear
    *Specify error output
    *Clear struktur

--- build.sh
+++ build.sh
@@ -1,63 +1,94 @@
 #!/bin/bash
+#########################################################################################################################
+#########################################################################################################################
 # Kernel Details
 VER="V1.1.4_axon7_alpha"
 BASE_AK_VER="Radioactive"
 
-# Paths (should be set as one of the first so that everything know where it belong to)
-KERNEL_DIR="${HOME}/NucleaROM/kernel/zte/msm8996"
-ZIP_MOVE="${HOME}/NucleaROM/releases"
-ZIMAGE_DIR="${HOME}/NucleaROM/kernel/zte/msm8996/arch/arm64/boot"
-MODULES_DIR="${HOME}/NucleaROM/lazyflasher/modules"
+# Set build folder
 BUILD_DIR="${HOME}/NucleaROM"
 
+BUILDER_NAME="ordenkrieger"
+BUILD_HOST_NAME="NuclearPowerPlant"
+
+# Set kernel source git link
+KSOURCE_GIT="https://github.com/OrdenKrieger/android_kernel_zte_msm8996.git -b radioactive"
+
+# Set kernel source folder
+KERNEL_DIR="$BUILD_DIR/kernel/zte/msm8996"
+ZIMAGE_DIR="$KERNEL_DIR/arch/arm64/boot" # Only change on soc/arch change ->arm64-arm etc.
+
+# Kernel Arch
+KERNEL_ARCH="arm64"
+
+# Set defconfig for the kernel
+KERNEL_DEFCONFIG="radioactive_defconfig"
+
+# Set kernel toolchain path
+TOOLCHAIN_DIR="$BUILD_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+
+# Set lazyflasher source git link
+LSOURCE_GIT="https://github.com/acuicultor/lazyflasher.git -b radioactive"
+LAZYFLASHER_DIR="$BUILD_DIR/lazyflasher" #No changes needed
+MODULES_DIR="$LAZYFLASHER_DIR/modules" #No changes needed
+
+# Set output folder
+ZIP_MOVE="$BUILD_DIR/releases"
+#########################################################################################################################
+#########################################################################################################################
+
+
+
+
 # Check if lazyflasher is present
-folder1="${HOME}/NucleaROM/lazyflasher/*.*"
+folder1="$LAZYFLASHER_DIR/*.*"
 if [ "$(ls -A $folder1)" ]; then
    echo -e "\E[1;32mLazyflasher is present"
    tput sgr0
 else
    echo -e "\E[1;31mDownloading lazyflasher dependencie"
    tput sgr0
-   git clone https://github.com/OrdenKrieger/lazyflasher.git $BUILD_DIR/lazyflasher
+   git clone $LSOURCE_GIT $LAZYFLASHER_DIR
 fi
 
 # Check if kernel source is present
-folder2="${HOME}/NucleaROM/kernel/zte/msm8996/*.*"
+folder2="$KERNEL_DIR/*.*"
 if [ "$(ls -A $folder2)" ]; then
    echo -e "\E[1;32mKernel source is present"
    tput sgr0
 else
    echo -e "\E[1;31mDownloading kernel source"
    tput sgr0
-   git clone https://github.com/OrdenKrieger/android_kernel_zte_msm8996.git $BUILD_DIR/kernel/zte/msm8996
+   git clone $KSOURCE_GIT $KERNEL_DIR
 fi
 
 # Cleanup remains of last builds
-git clean -d -f -x
-cd $BUILD_DIR/lazyflasher
-git clean -d -f -x
+cd $KERNEL_DIR # It's more safe to move first here. We don't want to clean any other git folder
+git clean -d -f -x > /dev/null 2>&1
+cd $LAZYFLASHER_DIR
+git clean -d -f -x > /dev/null 2>&1
 cd $KERNEL_DIR
 
 # Vars
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER=ordenkrieger
-export KBUILD_BUILD_HOST=NuclearPowerPlant
+export ARCH=$KERNEL_ARCH
+export SUBARCH=$KERNEL_ARCH
+export KBUILD_BUILD_USER=$BUILDER_NAME
+export KBUILD_BUILD_HOST=$BUILD_HOST_NAME
 
 # Build info 
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
-export CROSS_COMPILE="${HOME}/NucleaROM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export CROSS_COMPILE="$TOOLCHAIN_DIR"
 AK_VER="$BASE_AK_VER$VER"
 export LOCALVERSION=~`echo $AK_VER`
 export LOCALVERSION=~`echo $AK_VER`
-DEFCONFIG="radioactive_defconfig"
+DEFCONFIG="$KERNEL_DEFCONFIG"
 make $DEFCONFIG
 make $THREAD
 
 # Make our zips
-cp -vr $ZIMAGE_DIR/Image.gz-dtb $BUILD_DIR/lazyflasher/zImage
+cp -vr $ZIMAGE_DIR/Image.gz-dtb $LAZYFLASHER_DIR/zImage
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
-cd $BUILD_DIR/lazyflasher
+cd $LAZYFLASHER_DIR
 make VERSION=$VER NAME=$BASE_AK_VER
 
 # Cleanup remains of the kernel build (drivers etc.)
@@ -65,35 +96,56 @@ cd $KERNEL_DIR
 git clean -d -f -x > /dev/null 2>&1
 
 # Copy the ZIP to the Output
-cd $BUILD_DIR/lazyflasher
-mv $BUILD_DIR/lazyflasher/*.zip $ZIP_MOVE
-mv $BUILD_DIR/lazyflasher/*.sha1 $ZIP_MOVE
+cd $LAZYFLASHER_DIR
+mv $LAZYFLASHER_DIR/*.zip $ZIP_MOVE
+mv $LAZYFLASHER_DIR/*.sha1 $ZIP_MOVE
 
-# Message if success or fail
-file1="${HOME}/NucleaROM/releases/*.zip"
-file2="${HOME}/NucleaROM/lazyflasher/zImage"
-if [ -e $file1 ] &&  [ -e $file2 ]; then
+# Check if a zImage was created
+file1="$LAZYFLASHER_DIR/zImage"
+if [ -e $file1 ]; then
    echo -e ""
    echo -e ""
-   echo -e "\E[1;32mSuccess!"
-   echo -e "\E[1;32mYou can find the compiled kernel inside of $ZIP_MOVE"
-   echo -e ""
-   echo -e ""
+   echo -e "\E[1;32mThe zImage of the kernel was created successful"
    tput sgr0
 else
    echo -e ""
    echo -e ""
-   echo -e "\E[1;31mThere is no *.zip or zImage"
-   echo -e "\E[1;31mSomething must be gone wrong :("
-   echo -e "\E[1;31mCheck paths and kernel configuration and repeat the process"
-   echo -e "\E[1;31mCheck also if the kernel source and lazyflasher are in the right folder because the script only check if the folders are empty"
+   echo -e "\E[1;31mThere is no zImage created by the build process!"
+   echo -e "\E[1;31mCheck the the build history for errors and fix them in the kernel source."
+   tput sgr0
+fi
+
+# Check if lazyflasher made a .zip and this is working right
+file2="$ZIP_MOVE/*.zip"
+if [ -e $file1 ] &&  [ -e $file2 ]; then
+   echo -e "\E[1;32mThe ZIP is created successful"
+   echo -e ""
+   echo -e "\E[1;32mYou can find the compiled file inside of $ZIP_MOVE"
+   echo -e ""
+   echo -e "\E[1;32mSuccess!"
    echo -e ""
    echo -e ""
    tput sgr0
+else if [ ! -e $file1 ] ||  [ -e "$file2" ]; then
+   echo -e ""
+   echo -e "\E[1;31mAn error with the zImage appeared!" 
+   echo -e "\E[1;31mThe build process may have created a kernel.zip but this is corrupted."
+   echo -e ""
+   echo -e "\E[1;31mThe device won't boot if you flash it!"
+   echo -e ""
+   echo -e ""
+   tput sgr0
+else
+   echo -e "\E[1;31mThere is no kernel.zip created by the build process!"
+   echo -e "\E[1;31mCheck if lazyflasher is present and if the paths are set right in the build.sh."
+   echo -e ""
+   echo -e ""
+   tput sgr0
+fi
 fi
 
 # Cleanup remains of the *.zip build
 git clean -d -f -x > /dev/null 2>&1
 
 # Back to the roots
-cd $KERNEL_DIR
+cd $BUILD_DIR
